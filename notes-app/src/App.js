4586d026e71c7bf02d1e77dcb498b13289d5c0b4
Julia Banaszek <julia.m.banaszek@gmail.com>
2022-08-28 18:45:48 +0200

    Added: list counter & unique id

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,21 +3,24 @@ import ReactDOM from "react-dom/client";
 import Note from "./components/Note";
 import Form from "./components/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { nanoid } from "nanoid";
 
 function App(props) {
   const [notes, setNotes] = useState(props.notes);
   const notesList = notes.map((note) => (
     <Note id={note.id} name={note.name} key={note.id} />
   ));
+  const notesNoun = notesList.length !== 1 ? "notes" : "note";
+  const headingText = `You have ${notesList.length} ${notesNoun}`;
   function addNote(name) {
-    const newNote = { id: "id", name };
+    const newNote = { id: `note-${nanoid()}`, name };
     setNotes([...notes, newNote]);
   }
   return (
     <div className="container">
       <h1 className="title">Your notes</h1>
       <Form addNote={addNote} />
-      <h2>You have 3 notes</h2>
+      <h2>{headingText}</h2>
       <ul role="list" className="notes-list" aria-labelledby="notes-list">
         {notesList}
       </ul>
